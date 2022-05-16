9d35c4e9002803549c8aeace76cee10cf2664851
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-05-16 11:54:46 -0400

    Initial commit.

---
+++ edu.cuny.hunter.hybridize.ui/src/edu/cuny/hunter/hybridize/ui/plugins/HybridizeFunctionRefactoringPlugin.java
@@ -0,0 +1,32 @@
+package edu.cuny.hunter.hybridize.ui.plugins;
+
+import org.osgi.framework.BundleContext;
+
+import edu.cuny.citytech.refactoring.common.ui.RefactoringPlugin;
+
+public class HybridizeFunctionRefactoringPlugin extends RefactoringPlugin {
+
+	private static HybridizeFunctionRefactoringPlugin plugin;
+
+	public static RefactoringPlugin getDefault() {
+		return plugin;
+	}
+
+	@Override
+	protected String getRefactoringId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		plugin = this;
+		super.start(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+}
