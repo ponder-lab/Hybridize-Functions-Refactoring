a507d2f027c81b65895f9b921e3275cdf5da32a9
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2022-08-02 15:59:07 -0400

    Update A.py
    Add comments.

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridFalse/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridFalse/in/A.py
@@ -1,6 +1,6 @@
-def dummy_func():
+def dummy_func(): # Not hybrid because there is no decorator.
     pass
 
-@debug
+@debug # Also not hybrid. This time, there is a decorator, it's not tf.function.
 def dummy_func2():
     pass
