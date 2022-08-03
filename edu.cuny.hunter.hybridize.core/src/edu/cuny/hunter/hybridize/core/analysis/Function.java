c27b08c38ae485bb793a41f4e0b5374133523229
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2022-08-03 09:59:35 -0400

    fixing some errors with the merge

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/Function.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/Function.java
@@ -67,7 +67,6 @@ public class Function extends RefactorableProgramEntity {
 								}
 							}
 						}
-<<<<<<< HEAD
 					}
 					// If tf.function has arguments
 				} else if (decorator.func instanceof Call) {
@@ -91,8 +90,6 @@ public class Function extends RefactorableProgramEntity {
 								}
 							}
 						}
-=======
->>>>>>> c5e3b3a95e3dfc26822ab3b366b285ddfa2ba3e3
 					}
 				}
 			}
