fc877d970f257fb80c5bc61772a5abba810f01a5
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2022-08-03 12:37:31 -0400

    removing newline

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridMulitpleAttributes/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridMulitpleAttributes/in/A.py
@@ -6,7 +6,6 @@ import sys
 def dummy_fun():
     pass
 
-
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
 def dummy_test(test_input, expected):
     assert eval(test_input) == expected
