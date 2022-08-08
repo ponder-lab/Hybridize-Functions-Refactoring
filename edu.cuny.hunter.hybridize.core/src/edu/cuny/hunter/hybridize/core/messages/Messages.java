10845660a480ccdfcecd088981b3d27ab11d55df
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2022-08-08 13:13:29 -0400

    removing the messages that were repeated from common plugin

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/messages/Messages.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/messages/Messages.java
@@ -13,14 +13,11 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "edu.cuny.hunter.hybridize.core.messages.messages"; //$NON-NLS-1$
 	public static String CategoryDescription;
 	public static String CategoryName;
-	public static String CheckingPreconditions;
-	public static String CreatingChange;
 	public static String Name;
 	public static String NoFunctionsHavePassedThePreconditions;
 	public static String NoFunctionsToConvert;
 	public static String NoFunctionsToOptimize;
 	public static String PreconditionFailed;
-	public static String RefactoringNotPossible;
 	public static String FunctionsNotSpecified;
 	public static String CheckingFunctions;
 
