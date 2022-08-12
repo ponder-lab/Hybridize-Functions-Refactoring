3a7b383ef2715bb8ef74e7ade362baa9c7d80839
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-08-12 17:56:56 +0900

    Changes to System.out.println usage.
    - Remove some uses.
    - Replace some usages with logging.

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/Function.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/Function.java
@@ -1,14 +1,18 @@
 package edu.cuny.hunter.hybridize.core.analysis;
 
+import static org.eclipse.core.runtime.Platform.getLog;
+
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.python.pydev.parser.jython.SimpleNode;
 import org.python.pydev.parser.jython.ast.Attribute;
 import org.python.pydev.parser.jython.ast.Call;
-import org.python.pydev.parser.jython.ast.FunctionDef;
 import org.python.pydev.parser.jython.ast.ClassDef;
+import org.python.pydev.parser.jython.ast.FunctionDef;
 import org.python.pydev.parser.jython.ast.Name;
 import org.python.pydev.parser.jython.ast.NameTok;
 import org.python.pydev.parser.jython.ast.decoratorsType;
 import org.python.pydev.parser.visitors.NodeUtils;
-import org.python.pydev.parser.jython.SimpleNode;
 
 import edu.cuny.citytech.refactoring.common.core.RefactorableProgramEntity;
 
@@ -19,6 +23,8 @@ import edu.cuny.citytech.refactoring.common.core.RefactorableProgramEntity;
  * @author <a href="mailto:tcastrovelez@gradcenter.cuny.edu">Tatiana Castro Vélez</a>
  */
 public class Function extends RefactorableProgramEntity {
+
+	private static final ILog LOG = getLog(Function.class);
 
 	/**
 	 * The {@link FunctionDef} representing this {@link Function}.
@@ -52,12 +58,9 @@ public class Function extends RefactorableProgramEntity {
 				// If it is not an attribute then we cannot access it this way,
 				// therefore we need the if statement
 				if (decorator.func instanceof Attribute) { // e.g., tf.function
-					System.out.println(decorator);
 					Attribute decoratorFunction = (Attribute) decorator.func;
-					System.out.println(decoratorFunction);
 					if (decoratorFunction.value instanceof Name) {
 						Name decoratorName = (Name) decoratorFunction.value;
-						System.out.println(decoratorName);
 						if (decoratorName.id.equals("tf")) {
 							// We have a viable prefix. Get the attribute.
 							if (decoratorFunction.attr instanceof NameTok) {
@@ -65,20 +68,17 @@ public class Function extends RefactorableProgramEntity {
 								if (decoratorAttribute.id.equals("function")) {
 									// Found "tf.function."
 									this.isHybrid = true;
+									LOG.info(this + " is hybrid.");
 								}
 							}
 						}
 					}
 				} else if (decorator.func instanceof Call) { // e.g., tf.function(...)
-					System.out.println(decorator);
 					Call decoratorFunction = (Call) decorator.func;
-					System.out.println(decoratorFunction);
 					if (decoratorFunction.func instanceof Attribute) {
 						Attribute callFunction = (Attribute) decoratorFunction.func;
-						System.out.println(callFunction);
 						if (callFunction.value instanceof Name) {
 							Name decoratorName = (Name) callFunction.value;
-							System.out.println(decoratorName);
 							if (decoratorName.id.equals("tf")) {
 								// We have a viable prefix. Get the attribute.
 								if (callFunction.attr instanceof NameTok) {
@@ -86,6 +86,7 @@ public class Function extends RefactorableProgramEntity {
 									if (decoratorAttribute.id.equals("function")) {
 										// Found tf.function(...)
 										this.isHybrid = true;
+										LOG.info(this + " is hybrid.");
 									}
 								}
 							}
