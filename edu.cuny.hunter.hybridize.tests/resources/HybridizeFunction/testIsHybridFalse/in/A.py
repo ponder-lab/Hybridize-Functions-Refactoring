e1af05a6266734d674b55fa796b1d040852e94ac
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-08-24 04:50:20 -0600

    Use a decorator that actually exists.

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridFalse/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridFalse/in/A.py
@@ -1,6 +1,10 @@
 def dummy_func(): # Not hybrid because there is no decorator.
     pass
 
-@debug # Also not hybrid. This time, there is a decorator, it's not tf.function.
+@staticmethod # Also not hybrid. This time, there is a decorator, it's not tf.function.
 def dummy_func2():
     pass
+
+if __name__ == '__main__':
+    dummy_func()
+    dummy_func2()
