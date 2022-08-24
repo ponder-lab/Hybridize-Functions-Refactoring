b7c63f5d1931679642e015d31c4cb1ca805aa842
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-08-24 03:17:28 -0600

    Call the functions.

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testFQN/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testFQN/in/A.py
@@ -26,3 +26,12 @@ class Class1:
         def func_class4(self):
             pass
 
+if __name__ == "__main__":
+    func()
+    func1()
+    Class1.func_class1()
+    c = Class1()
+    c.func_class3()
+    Class1.Class2.func_class2()
+    c2 = Class1.Class2()
+    c2.func_class4()
