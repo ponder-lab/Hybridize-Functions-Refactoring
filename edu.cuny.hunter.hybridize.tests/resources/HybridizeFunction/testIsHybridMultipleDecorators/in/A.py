3aaa1fa6476726e5796de5c3922cc890dc2109a3
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-08-25 18:33:25 -0400

    Remove trailing WS.

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridMultipleDecorators/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testIsHybridMultipleDecorators/in/A.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 from tf_image.core.convert_type_decorator import convert_type
 
 @tf.function
-@convert_type 
+@convert_type
 def test():
     pass
 
-@convert_type 
+@convert_type
 @tf.function
 def test():
     pass
