ef59c86768f5945e7edf486018254600446ce63f
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2022-09-05 22:05:05 -0400

    formatting the python files for the new lines

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testComputeParameters/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testComputeParameters/in/A.py
@@ -1,35 +1,43 @@
 import tensorflow as tf
 
-@tf.function(input_signature=(tf.TensorSpec(shape=[None], dtype=tf.float32),), autograph= False)
+
+@tf.function(input_signature=(tf.TensorSpec(shape=[None], dtype=tf.float32),), autograph=False)
 def func(x):
   print('Tracing with', x)
   return x
+
 
 @tf.function(experimental_autograph_options=tf.autograph.experimental.Feature.EQUALITY_OPERATORS)
 def func1(i):
   if i == 0:  # EQUALITY_OPERATORS allows the use of == here.
     tf.print('i is zero')
 
+
 @tf.function(experimental_follow_type_hints=True)
 def func2(x: tf.Tensor):
   print('Tracing')
   return x
 
+
 @tf.function(experimental_implements="google.matmul_low_rank_matrix")
 def func3():
     pass
+
   
 @tf.function(jit_compile=True)
 def func4():
     print("Tracing")
 
+
 @tf.function(reduce_retracing=True)
 def func5(x, y):
   return x ** 2 + y
+
   
 @tf.function(experimental_compile=True)
 def func6():
      print("Tracing")
+
  
 if __name__ == '__main__':
     number = tf.constant([1.0, 1.0])
@@ -42,4 +50,5 @@ if __name__ == '__main__':
     func4()
     func5(x, y)
     func6()
+    
     
