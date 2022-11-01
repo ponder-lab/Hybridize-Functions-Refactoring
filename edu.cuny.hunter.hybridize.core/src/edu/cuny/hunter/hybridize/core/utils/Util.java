ce26111922c777240260b0bc34164d8fc771e4c7
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-11-01 12:19:02 -0400

    Start the piping from the UI.
    The test code has the piping complete.

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/utils/Util.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/utils/Util.java
@@ -1,16 +1,21 @@
 package edu.cuny.hunter.hybridize.core.utils;
 
+import java.util.Set;
+
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.participants.ProcessorBasedRefactoring;
-import org.python.pydev.parser.jython.ast.FunctionDef;
-
+import org.python.pydev.ast.refactoring.TooManyMatchesException;
+import org.python.pydev.core.IPythonNature;
+import edu.cuny.hunter.hybridize.core.analysis.FunctionDefinition;
 import edu.cuny.hunter.hybridize.core.refactorings.HybridizeFunctionRefactoringProcessor;
 
 public class Util {
 
-	public static Refactoring createRefactoring(FunctionDef[] functions, IProgressMonitor monitor) {
-		HybridizeFunctionRefactoringProcessor processor = new HybridizeFunctionRefactoringProcessor(functions, monitor);
+	public static Refactoring createRefactoring(Set<FunctionDefinition> functionDefinitions, IPythonNature nature, IProgressMonitor monitor)
+			throws TooManyMatchesException, BadLocationException {
+		HybridizeFunctionRefactoringProcessor processor = new HybridizeFunctionRefactoringProcessor(functionDefinitions, nature, monitor);
 		return new ProcessorBasedRefactoring(processor);
 	}
 
