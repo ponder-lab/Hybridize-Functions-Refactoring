a27248405919ccf8bbbf4a44468f156a2529abb2
Raffi Khatchaodourian <raffi.khatchadourian@hunter.cuny.edu>
2022-11-01 11:21:25 -0400

    Actually add the class.

---
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/FunctionDefinition.java
@@ -0,0 +1,42 @@
+package edu.cuny.hunter.hybridize.core.analysis;
+
+import java.io.File;
+import java.util.Objects;
+
+import org.eclipse.jface.text.IDocument;
+import org.python.pydev.parser.jython.ast.FunctionDef;
+
+public class FunctionDefinition {
+	FunctionDef functionDef;
+	String containingModuleName;
+	File containingFile;
+	IDocument containingDocument;
+
+	public FunctionDefinition(FunctionDef functionDef, String containingModuleName, File containingFile, IDocument containingDocument) {
+		this.functionDef = functionDef;
+		this.containingModuleName = containingModuleName;
+		this.containingFile = containingFile;
+		this.containingDocument = containingDocument;
+	}
+
+	@Override
+	public int hashCode() {
+		return functionDef.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null)
+			return false;
+
+		if (getClass() != obj.getClass())
+			return false;
+
+		FunctionDefinition other = (FunctionDefinition) obj;
+
+		return Objects.equals(functionDef, other.functionDef);
+	}
+}
