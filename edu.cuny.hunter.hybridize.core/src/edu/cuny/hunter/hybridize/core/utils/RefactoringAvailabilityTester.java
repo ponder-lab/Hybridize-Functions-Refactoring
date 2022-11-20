8116adbb73653562caeaf7867321a1a662c36d78
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2022-11-20 14:28:04 -0500

    fixing checkstyle violation

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/utils/RefactoringAvailabilityTester.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/utils/RefactoringAvailabilityTester.java
@@ -4,6 +4,7 @@ import static org.eclipse.core.runtime.Platform.getLog;
 
 import org.eclipse.core.runtime.ILog;
 import org.python.pydev.parser.jython.ast.FunctionDef;
+
 import edu.cuny.hunter.hybridize.core.analysis.Util;
 
 public class RefactoringAvailabilityTester {
