e72802a0e8c890c6d6da1cdb6b8a737f43707723
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2022-11-20 14:22:28 -0500

    changes to obtain more useful information

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/utils/RefactoringAvailabilityTester.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/utils/RefactoringAvailabilityTester.java
@@ -4,13 +4,14 @@ import static org.eclipse.core.runtime.Platform.getLog;
 
 import org.eclipse.core.runtime.ILog;
 import org.python.pydev.parser.jython.ast.FunctionDef;
+import edu.cuny.hunter.hybridize.core.analysis.Util;
 
 public class RefactoringAvailabilityTester {
 
 	private static final ILog LOG = getLog(RefactoringAvailabilityTester.class);
 
 	public static boolean isHybridizationAvailable(FunctionDef function) {
-		LOG.info("Testing hybridization availability for: " + function);
+		LOG.info("Testing hybridization availability for: " + Util.getQualifiedName(function));
 		// TODO
 		return true;
 	}
