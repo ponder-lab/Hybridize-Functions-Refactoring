96b6ad2b1146f30450a50f11e049efc05104ad19
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-01-16 23:33:46 -0500

    fixing assert

--- edu.cuny.hunter.hybridize.tests/test cases/edu/cuny/hunter/hybridize/tests/HybridizeFunctionRefactoringTest.java
+++ edu.cuny.hunter.hybridize.tests/test cases/edu/cuny/hunter/hybridize/tests/HybridizeFunctionRefactoringTest.java
@@ -1292,7 +1292,6 @@ public class HybridizeFunctionRefactoringTest extends RefactoringTest {
 			Function function = functions.iterator().next();
 			assertNotNull(function);
 			assertFalse(function.isHybrid());
-
 		}
 
 		assertEquals(2, count);
