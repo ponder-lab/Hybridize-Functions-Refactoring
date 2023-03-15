d671a92bec605909f26b12b0643b8ea8734bc577
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-03-15 15:11:46 -0400

    Make it look like the example from TF docs.

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testModel/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testModel/in/A.py
@@ -16,7 +16,7 @@ class SequentialModel(tf.keras.Model):
     self.dropout = tf.keras.layers.Dropout(0.2)
     self.dense_2 = tf.keras.layers.Dense(10)
 
-  def call(self, x):
+  def __call__(self, x):
     x = self.flatten(x)
 
     for layer in self.my_layers:
