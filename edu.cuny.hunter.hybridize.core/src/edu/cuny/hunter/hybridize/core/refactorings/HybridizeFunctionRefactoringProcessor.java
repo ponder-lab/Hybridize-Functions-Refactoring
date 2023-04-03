84ff36cd2bd74fa6280b43e9bbf5c942510f5a80
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-04-03 17:07:00 -0400

    Add missing subMonitor call.

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/refactorings/HybridizeFunctionRefactoringProcessor.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/refactorings/HybridizeFunctionRefactoringProcessor.java
@@ -121,6 +121,8 @@ public class HybridizeFunctionRefactoringProcessor extends RefactoringProcessor 
 
 			status.merge(checkParameters(func));
 			status.merge(checkDecorators(func));
+
+			subMonitor.worked(1);
 		}
 
 		return status;
