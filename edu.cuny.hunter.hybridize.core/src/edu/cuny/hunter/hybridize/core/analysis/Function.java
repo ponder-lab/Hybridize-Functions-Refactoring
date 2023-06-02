f53ffd92c70a2bf4507e2a7ea6773734e8b7418b
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-06-02 15:00:47 -0400

    Add missing else.

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/Function.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/analysis/Function.java
@@ -34,8 +34,8 @@ public class Function extends RefactorableProgramEntity {
 
 	/**
 	 * Parameters that may be passed to a tf.fuction decorator. Parameter descriptions found at:
-	 * https://tensorflow.org/versions/r2.9/api_docs/python/tf/function Note: We are also parsing the deprecated parameters specified in
-	 * the documentation. Users can still use these deprecated parameters. Therefore we need to be able to account for them. Please refer to
+	 * https://tensorflow.org/versions/r2.9/api_docs/python/tf/function Note: We are also parsing the deprecated parameters specified in the
+	 * documentation. Users can still use these deprecated parameters. Therefore we need to be able to account for them. Please refer to
 	 * https://github.com/ponder-lab/Hybridize-Functions-Refactoring/wiki/tf.function-parameter's-version-information to see more
 	 * information about the tf.function parameters according to the versions.
 	 */
@@ -123,7 +123,7 @@ public class Function extends RefactorableProgramEntity {
 				}
 			} // We expect to have the last tf.function decorator in tfFunctionDecorator
 
-			if (tfFunctionDecorator != null)
+			if (tfFunctionDecorator != null) {
 				// tfFunctionDecorator must be an instance of Call, because that's the only way we have parameters.
 				if (tfFunctionDecorator.func instanceof Call) {
 					Call callFunction = (Call) tfFunctionDecorator.func;
@@ -167,8 +167,8 @@ public class Function extends RefactorableProgramEntity {
 						}
 					}
 				} // else, tf.function is used without parameters.
-
-			throw new IllegalStateException("No decorator exists. Can't compute decorator parameter existance.");
+			} else
+				throw new IllegalStateException("No decorator exists. Can't compute decorator parameter existance.");
 		}
 
 		/**
