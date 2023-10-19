675aa045ebbc908c39ee3cfdb58897f1945f6e8a
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-10-19 17:17:24 -0400

    Simplify test.

--- edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testPythonSideEffects9/in/A.py
+++ edu.cuny.hunter.hybridize.tests/resources/HybridizeFunction/testPythonSideEffects9/in/A.py
@@ -1,11 +1,5 @@
-# From https://www.tensorflow.org/guide/function#executing_python_side_effects.
-
-import tensorflow as tf
-
-
 def f(x):
   print("Traced with", x)
-  tf.print("Executed with", x)
 
 
 f(1)
