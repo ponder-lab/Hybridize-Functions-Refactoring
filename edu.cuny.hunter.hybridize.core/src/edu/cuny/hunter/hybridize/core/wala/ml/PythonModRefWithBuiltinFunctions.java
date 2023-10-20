ea95aa6c3a0905cc9e732e3e70f26c8647e6649e
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-10-20 15:37:56 -0400

    Handle side-effects from print().

--- edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/wala/ml/PythonModRefWithBuiltinFunctions.java
+++ edu.cuny.hunter.hybridize.core/src/edu/cuny/hunter/hybridize/core/wala/ml/PythonModRefWithBuiltinFunctions.java
@@ -2,13 +2,10 @@ package edu.cuny.hunter.hybridize.core.wala.ml;
 
 import java.util.Collection;
 
-import com.ibm.wala.cast.ir.ssa.AstGlobalWrite;
+import com.ibm.wala.cast.ipa.callgraph.AstGlobalPointerKey;
 import com.ibm.wala.cast.ir.ssa.AstLexicalAccess.Access;
 import com.ibm.wala.cast.ir.ssa.AstLexicalRead;
-import com.ibm.wala.cast.ir.ssa.AstLexicalWrite;
-import com.ibm.wala.cast.ir.ssa.AstPropertyWrite;
 import com.ibm.wala.cast.python.modref.PythonModRef;
-import com.ibm.wala.cast.python.modref.PythonModRef.PythonModVisitor;
 import com.ibm.wala.cast.python.ssa.PythonInvokeInstruction;
 import com.ibm.wala.cast.python.types.PythonTypes;
 import com.ibm.wala.classLoader.IClass;
@@ -17,16 +14,21 @@ import com.ibm.wala.ipa.callgraph.propagation.InstanceKey;
 import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis;
 import com.ibm.wala.ipa.callgraph.propagation.PointerKey;
 import com.ibm.wala.ipa.modref.ExtendedHeapModel;
-import com.ibm.wala.shrike.shrikeBT.IInvokeInstruction.IDispatch;
 import com.ibm.wala.ssa.SSAInstruction;
-import com.ibm.wala.ssa.SSAInvokeInstruction;
-import com.ibm.wala.types.MethodReference;
 import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.intset.OrdinalSet;
 
 public class PythonModRefWithBuiltinFunctions extends PythonModRef {
 
 	public static class PythonModVisitorWithBuiltinFunctions<T extends InstanceKey> extends PythonModVisitor<T> {
+
+		private static final AstGlobalPointerKey GLOBAL_OUTPUT_STREAM_POINTER_KEY = new AstGlobalPointerKey(
+				PythonModVisitorWithBuiltinFunctions.class.getPackageName().replace('.', '/') + "/OUT");
+
+		private static final String PRINT_FUNCTION_VARIABLE_NAME = "print";
+
+		private static final TypeReference PRINT_FUNCTION_TYPE_REFERENCE = TypeReference.findOrCreate(PythonTypes.pythonLoader,
+				"Lwala/builtin/" + PRINT_FUNCTION_VARIABLE_NAME);
 
 		public PythonModVisitorWithBuiltinFunctions(CGNode n, Collection<PointerKey> result, ExtendedHeapModel h, PointerAnalysis<T> pa,
 				boolean ignoreAllocHeapDefs) {
@@ -42,17 +44,19 @@ public class PythonModRefWithBuiltinFunctions extends PythonModRef {
 				AstLexicalRead read = (AstLexicalRead) def;
 				Access[] accesses = read.getAccesses();
 
-				if (accesses.length > 0 && accesses[0].variableName.equals("str")) {
+				if (accesses.length > 0 && accesses[0].variableName.equals(PRINT_FUNCTION_VARIABLE_NAME)) {
 					PointerKey pk = this.h.getPointerKeyForLocal(this.n, use);
 					OrdinalSet<T> pointsToSet = this.pa.getPointsToSet(pk);
 
 					pointsToSet.forEach(ik -> {
 						IClass concreteType = ik.getConcreteType();
-						TypeReference reference = concreteType.getReference();
+						TypeReference typeReference = concreteType.getReference();
 
-						TypeReference strFunction = TypeReference.findOrCreate(PythonTypes.pythonLoader, "Lwala/builtin/str");
-						System.out.println(reference.equals(strFunction));
-
+						if (typeReference.equals(PRINT_FUNCTION_TYPE_REFERENCE)) {
+							// found a call to the built-in print function, which has side effects.
+							// add a pointer to a fake global variable representing a modification to the output stream.
+							this.result.add(GLOBAL_OUTPUT_STREAM_POINTER_KEY);
+						}
 					});
 				}
 			}
