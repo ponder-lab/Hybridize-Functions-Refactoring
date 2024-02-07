cbf8de4c44db3054d4d4f903055e565a30ade5c8
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-02-07 15:15:03 -0500

    Fix new test.

--- edu.cuny.hunter.hybridize.tests/test cases/edu/cuny/hunter/hybridize/tests/HybridizeFunctionRefactoringTest.java
+++ edu.cuny.hunter.hybridize.tests/test cases/edu/cuny/hunter/hybridize/tests/HybridizeFunctionRefactoringTest.java
@@ -4977,7 +4977,7 @@ public class HybridizeFunctionRefactoringTest extends RefactoringTest {
 
 			switch (f.getIdentifier()) {
 			case "f":
-			case "g":
+			case "C.g":
 				assertEquals(s, f.getHasPythonSideEffects());
 				break;
 
