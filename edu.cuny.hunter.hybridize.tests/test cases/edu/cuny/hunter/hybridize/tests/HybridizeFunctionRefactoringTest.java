2107422dde0bc40fefe1f918e2bde668f7a3b7b7
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-02-08 10:29:50 -0500

    Remove repetitive code.

--- edu.cuny.hunter.hybridize.tests/test cases/edu/cuny/hunter/hybridize/tests/HybridizeFunctionRefactoringTest.java
+++ edu.cuny.hunter.hybridize.tests/test cases/edu/cuny/hunter/hybridize/tests/HybridizeFunctionRefactoringTest.java
@@ -5677,8 +5677,6 @@ public class HybridizeFunctionRefactoringTest extends RefactoringTest {
 		for (Function function : functionSet) {
 			switch (function.getIdentifier()) {
 			case "f":
-				functionToExpectedSideEffects.put(function, true);
-				break;
 			case "g":
 				functionToExpectedSideEffects.put(function, true);
 				break;
