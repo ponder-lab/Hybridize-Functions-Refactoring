b4ca5e107c6aa18f904a3f7c850f1ff5e56e1950
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-05-02 12:39:50 -0400

    Add script to update XML summaries.

---
+++ edu.cuny.hunter.hybridize.core/update_summaries.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+ML_PATH=$HOME/ML
+
+cp $ML_PATH/com.ibm.wala.cast.python/data/flask.xml .
+cp $ML_PATH/com.ibm.wala.cast.python/data/functools.xml .
+cp $ML_PATH/com.ibm.wala.cast.python/data/pandas.xml .
+cp $ML_PATH/com.ibm.wala.cast.python/data/pytest.xml .
+cp $ML_PATH/com.ibm.wala.cast.python.ml/data/tensorflow.xml .
