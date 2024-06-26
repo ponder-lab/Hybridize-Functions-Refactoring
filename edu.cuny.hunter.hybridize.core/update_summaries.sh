95ec16cf3848693f94ff0add751ae0e97c6d5233
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-06-25 23:28:21 -0400

    Add abseil.

--- edu.cuny.hunter.hybridize.core/update_summaries.sh
+++ edu.cuny.hunter.hybridize.core/update_summaries.sh
@@ -8,4 +8,5 @@ cp $ML_PATH/com.ibm.wala.cast.python/data/functools.xml .
 cp $ML_PATH/com.ibm.wala.cast.python/data/pandas.xml .
 cp $ML_PATH/com.ibm.wala.cast.python/data/pytest.xml .
 cp $ML_PATH/com.ibm.wala.cast.python/data/click.xml .
+cp $ML_PATH/com.ibm.wala.cast.python/data/abseil.xml .
 cp $ML_PATH/com.ibm.wala.cast.python.ml/data/tensorflow.xml .
